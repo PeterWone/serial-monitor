bf975f4aca17555d97b951e875b12476ddf1c3c7
Peter Wone <peter.wone@outlook.com>
2020-10-26 11:04:32 +1000

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace serial_monitor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var serial = new SerialPort(args[0], int.Parse(args[1]));
+      Console.CancelKeyPress += delegate
+      {
+        serial.Close();
+        serial.Dispose();
+      };
+      serial.DataReceived += (object sender, SerialDataReceivedEventArgs e) => Console.Write(serial.ReadExisting());
+      serial.Open();
+      Console.WriteLine($"Monitoring {args[0]} at {args[1]}");
+      while (true) Thread.Sleep(20);
+    }
+  }
+}
